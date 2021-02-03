742dcf9dd3378b2cee8171bed226909995bdb1e3
dshomny0 <dshomny0@email.cpcc.edu>
2021-02-03 21:37:18 +0100

    Create HellowWorld Program
    Added another print statement.

---
+++ Helloworld.java
@@ -0,0 +1,20 @@
+//HelloWorld
+
+   public class Helloworld {
+   
+      public static void main(String [] args) {
+      
+         System.out.println("Hello world!");
+         
+            String name = "George Washington";
+            
+            System.out.println(name);
+            
+            int age  =  24;
+            
+            System.out.println(age);
+         
+         
+         }
+         
+    }
